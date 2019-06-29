33a55216d7111033dee69432946d7d9c4fef43e3
AltugAltintas <altuga@gmail.com>
2019-06-29 12:55:24 +0300

    Part 1 - chapter 2

--- micro/src/main/java/com/airhacks/ping/boundary/PingResource.java
+++ micro/src/main/java/com/airhacks/ping/boundary/PingResource.java
@@ -1,5 +1,7 @@
 package com.airhacks.ping.boundary;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -7,12 +9,16 @@ import javax.ws.rs.Path;
  *
  * @author airhacks.com
  */
+@RequestScoped
 @Path("ping")
 public class PingResource {
+    
+    @Inject 
+    PingService pingService;
 
     @GET
     public String ping() {
-        return "Enjoy Java EE 8!";
+        return pingService.getMessage();
     }
 
 }
